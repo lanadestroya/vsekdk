e86a3fc74c0782ff71d9648b2455fca3d78e00b5
lanadestroya <carlzksaver20@gmail.com>
2025-05-13 19:09:13 +0500

    base64.4

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -2,7 +2,12 @@ const ApiError = require('../error/ApiError');
 
 module.exports = function (err, req, res, next) {
     if (err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
+        return res.status(err.status).json({ message: err.message })
     }
-    return res.status(500).json({message: "Непредвиденная ошибка!"})
+    // Возвращаем подробности ошибки!
+    return res.status(500).json({ 
+        message: "Непредвиденная ошибка!",
+        error: err.message,
+        stack: err.stack
+    })
 }
