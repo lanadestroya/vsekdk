90f43fc165f85d6826bccebdad77d71d020270e5
unsaved1 <eelwrld@gmail.com>
2025-06-18 15:02:01 +0500

    update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 const { Role } = require('./models/models')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5003
 
 const app = express()
 
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }))
 
 // Статические файлы
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(express.static(path.join(__dirname, './client')))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // API routes
@@ -30,7 +30,7 @@ app.use('/api', router)
 
 // Обработка всех остальных запросов
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'index.html'))
+    res.sendFile(path.join(__dirname, './client', 'index.html'))
 })
 
 // Error handling
